c28a451ad22db8c94b0830ea1faa0cad4a50677b
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2016-07-05 20:57:40 +0200

    fix force reset

--- background/public/sw.js
+++ background/public/sw.js
@@ -1,19 +1,16 @@
 this.addEventListener('install', function(event) {
-  console.log('install');
   event.waitUntil(self.skipWaiting());
 });
 
 this.addEventListener("activate", function(event) {
-  event.waitUntil(self.clients.claim());
-  console.log('activated');
+  event.waitUntil(self.clients.claim().then(function () {
+    importScripts('/socket.io/socket.io.js');
+    var socket = io.connect('http://localhost:3000', {
+        jsonp: false
+    });
 
-  importScripts('/socket.io/socket.io.js');
-  var socket = io.connect('http://localhost:3000', {
-      jsonp: false
-  });
-
-  setInterval(function () {
-    socket.emit('timer', "3PO!, come in 3P0! shut down the gargabe mashers!");
-  }, 3000);
-
+    setInterval(function () {
+      socket.emit('timer', (new Date).toLocaleTimeString() + ": 3PO!, come in 3P0! shut down the gargabe mashers!");
+    }, 3000);
+  }));
 });
